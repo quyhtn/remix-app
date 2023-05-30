47b2cdcc7c6595aa87fb500d5248080cd95c90b0
quyhuynh <quyhuynh@chidoanh.com>
2023-05-30 10:23:07 +0700

    first commit

---
+++ app/root.jsx
@@ -0,0 +1,108 @@
+import {
+  Link,
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+  isRouteErrorResponse,
+  useRouteError,
+} from "@remix-run/react";
+
+import styles from '~/styles/main.css';
+import MainNavigation from "./components/MainNavigation";
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <header>
+          <MainNavigation />
+        </header>
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
+
+export const meta = () => ([
+  {
+    charset: 'utf-8'
+  },
+  { title: 'New Remix App' },
+  {
+    viewport: 'width=device-width,initial-scale=1'
+  }
+]);
+
+// this ErrorBoundary will be rendered by Remix instead (!) of the "App" component (if an error is thrown anywhere in your app)
+export function ErrorBoundary() {
+  const error = useRouteError();
+
+  if (isRouteErrorResponse(error)) {
+    return (
+      <html lang="en">
+        <head>
+          <Meta />
+          <Links />
+          <title>{error.statusText}</title>
+        </head>
+        <body>
+          <header>
+            <MainNavigation />
+          </header>
+          <main className="error">
+            <h1>{error.statusText}</h1>
+            <p>{error.data?.message || 'Something went wrong!'}</p>
+            <p>
+              Back to <Link to="/">safety</Link>!
+            </p>
+          </main>
+          <ScrollRestoration />
+          <Scripts />
+          <LiveReload />
+        </body>
+      </html>
+    );
+  }
+
+
+  return (
+    <html lang="en">
+      <head>
+        <Meta />
+        <Links />
+        <title>An error occurred!</title>
+      </head>
+      <body>
+        <header>
+          <MainNavigation />
+        </header>
+        <main className="error">
+          <h1>An error occurred!</h1>
+          <p>{error.message}</p>
+          <p>
+            Back to <Link to="/">safety</Link>!
+          </p>
+        </main>
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
+
+export function links() {
+  return [{ rel: 'stylesheet', href: styles }];
+}
